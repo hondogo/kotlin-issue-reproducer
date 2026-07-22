bd7f646fc40f3e9eb9ad7e13481755630d9439f3
Pavel <noreply@example.com>
2026-07-22 16:33:52 +0300

    update Kotlin and Compose verions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("multiplatform") version "2.4.0-Beta1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0-Beta1" apply false
-    id("org.jetbrains.compose") version "1.11.0-beta01" apply false
+    kotlin("multiplatform") version "2.4.20-Beta2" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20-Beta2" apply false
+    id("org.jetbrains.compose") version "1.12.0-beta02" apply false
 }
 
 allprojects {
